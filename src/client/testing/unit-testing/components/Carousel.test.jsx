849f8ea77d4f8acb8b8ff075b688a7f9a3ce54a8
AUDREY KADJAR <audreykadjar@gmail.com>
2023-01-09 21:28:56 +0100

    unit testing done

--- src/client/testing/unit-testing/components/Carousel.test.jsx
+++ src/client/testing/unit-testing/components/Carousel.test.jsx
@@ -1,20 +1,16 @@
 import React from 'react';
 import 'intersection-observer';
 import {
-  render, screen, cleanup, within, fireEvent,
+  render, screen, within, fireEvent,
 } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
-import Carousel from '../../components/Carousel';
+import Carousel from '../../../components/Carousel';
 import '@testing-library/jest-dom';
 
 const mockImagesList = [{ id: '120', poster_path: '0.jpg', original_title: 'movie 0' }, { id: '121', poster_path: '1.jpg', original_title: 'movie 1' }, { id: '122', poster_path: '2.jpg', original_title: 'movie 2' }, { id: '123', poster_path: '3.jpg', original_title: 'movie 3' }, { id: '124', poster_path: '4.jpg', original_title: 'movie 4' }, { id: '125', poster_path: '5.jpg', original_title: 'movie 5' }, { id: '126', poster_path: '6.jpg', original_title: 'movie 6' }, { id: '127', poster_path: '7.jpg', original_title: 'movie 7' }, { id: '128', poster_path: '8.jpg', original_title: 'movie 8' }, { id: '129', poster_path: '9.jpg', original_title: 'movie 9' }];
 
-describe('a carousel component that displays a scrollable list of images', () => {
-  afterEach(() => {
-    cleanup();
-  });
-
-  it('displays a scrollable list', async () => {
+describe('carousel displays a scrollable list of interactive images', () => {
+  it('displays a scrollable list of items', async () => {
     const { getByTestId } = render(<Carousel movieList={mockImagesList} genre="comedy" />, { wrapper: MemoryRouter });
 
     const carouselElement = getByTestId('carousel-element');
@@ -26,14 +22,14 @@ describe('a carousel component that displays a scrollable list of images', () =>
     expect(items.length).toBe(10);
   });
 
-  it('on click, each carousel item navigates to its detail page', async () => {
+  it('on click, each item navigates to its detail page', async () => {
     const { getAllByTestId } = render(<Carousel movieList={mockImagesList} genre="comedy" />, { wrapper: MemoryRouter });
 
     const imageItem = getAllByTestId('carousel-item-button');
     fireEvent.click(imageItem[0]);
-      
-      (() => {
-          expect(screen.getByText(imageItem[0].original_title)).toBeInTheDocument();
-      })
-    })
+
+    (() => {
+      expect(screen.getByText(imageItem[0].original_title)).toBeInTheDocument();
+    });
+  });
 });
---
+++ src/client/testing/unit-testing/components/PosterImage.test.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import 'intersection-observer';
+import {
+  render, screen,
+} from '@testing-library/react';
+import PosterImage from '../../../components/PosterImage';
+import '@testing-library/jest-dom';
+
+const imageBaseUrl = 'https://image.tmdb.org/t/p/w500';
+const posterPathMock = '/0.jpg';
+
+test('displays an image composing a path with its prop', () => {
+  render(<PosterImage posterPath={posterPathMock} />);
+
+  const image = screen.getByRole('img');
+  expect(image.src).toBe(`${imageBaseUrl}${posterPathMock}`);
+  expect(image.alt).toBe('movie poster');
+});
